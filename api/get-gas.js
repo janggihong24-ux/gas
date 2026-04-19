9f5b36433e48b6684451dab8092a28ce08547d47
janggihong24-ux <janggihong24@gmail.com>
2026-04-19 16:55:32 +0900

    Update get-gas.js

--- api/get-gas.js
+++ api/get-gas.js
@@ -2,40 +2,42 @@ const axios = require('axios');
 const { XMLParser } = require('fast-xml-parser');
 
 module.exports = async (req, res) => {
-    // 브라우저 접속 허용(CORS)
+    // 브라우저 접속 허용(CORS) 및 한글 설정
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
     try {
+        // 질문하신 전체 주소를 여기에 정확히 넣었습니다.
         const opinetUrl = 'https://www.opinet.co.kr/api/detailById.do?code=F260409864&id=A0027320&out=xml';
+        
         const response = await axios.get(opinetUrl);
-        
         const parser = new XMLParser();
-        const result = parser.parse(response.data);
+        const jsonObj = parser.parse(response.data);
         
-        // 오피넷 XML 구조에서 필요한 데이터 추출
-        const oilInfo = result.RESULT.OIL;
+        // 오피넷 XML 데이터 구조: RESULT > OIL (보통 배열 형태)
+        const oilInfo = jsonObj.RESULT?.OIL;
         
-        // 경유(D047) 가격 찾기 (보통 리스트 형태로 오므로 찾기가 필요함)
-        // 만약 리스트가 아니라 단일 객체라면 바로 접근
-        const gasStationName = oilInfo[0]?.OS_NM || oilInfo.OS_NM;
-        const updateDate = oilInfo[0]?.UPDT_DT || oilInfo.UPDT_DT;
-        
-        // 경유 가격 추출 (제품구분 D047이 경유)
-        let dieselPrice = "정보 없음";
-        if (Array.isArray(oilInfo)) {
-            const dieselData = oilInfo.find(item => item.PRODCD === 'D047');
-            if (dieselData) dieselPrice = dieselData.PRICE;
-        } else if (oilInfo.PRODCD === 'D047') {
-            dieselPrice = oilInfo.PRICE;
+        if (!oilInfo) {
+            return res.status(200).json({ name: "데이터 없음", price: "0", date: "-" });
         }
 
+        let targetData = {};
+
+        // 데이터가 배열이면 경유(D047)를 찾고, 아니면 단일 객체 사용
+        if (Array.isArray(oilInfo)) {
+            targetData = oilInfo.find(item => item.PRODCD === 'D047') || oilInfo[0];
+        } else {
+            targetData = oilInfo;
+        }
+
+        // 화면에 전달할 최종 데이터
         res.status(200).json({
-            name: gasStationName,
-            price: dieselPrice,
-            date: updateDate
+            name: targetData.OS_NM || "주유소명 없음",
+            price: targetData.PRICE || 0,
+            date: String(targetData.UPDT_DT || "-")
         });
+
     } catch (error) {
-        res.status(500).json({ error: '데이터 추출 실패', detail: error.message });
+        res.status(500).json({ error: '서버 에러 발생', detail: error.message });
     }
 };
